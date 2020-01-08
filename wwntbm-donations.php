7350cdf246e4e40cdd336a41ba1fe2e964c0f2b0
Andrew Minion <andrew@andrewrminion.com>
2020-01-07 21:00:51 -0600

    remove acf json

--- wwntbm-donations.php
+++ wwntbm-donations.php
@@ -119,27 +119,3 @@ function wwntbm_missionary_donation_button( $content ) {
 	return $content;
 }
 add_filter( 'the_content', 'wwntbm_missionary_donation_button' );
-
-
-/**
- * Set ACF local JSON save directory
- *
- * @param  string $path ACF local JSON save directory.
- * @return string ACF local JSON save directory
- */
-function wwntbm_missionary_acf_json_save_point( $path ) {
-	return plugin_dir_path( __FILE__ ) . '/acf-json';
-}
-add_filter( 'acf/settings/save_json', 'wwntbm_missionary_acf_json_save_point' );
-
-/**
- * Set ACF local JSON open directory
- *
- * @param  array $path ACF local JSON open directory.
- * @return array ACF local JSON open directory
- */
-function wwntbm_missionary_acf_json_load_point( $path ) {
-	$paths[] = plugin_dir_path( __FILE__ ) . '/acf-json';
-	return $paths;
-}
-add_filter( 'acf/settings/load_json', 'wwntbm_missionary_acf_json_load_point' );
